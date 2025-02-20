df0f1bf015c0f2a6d194b5cc431c9e2d0593f2c2
Obito2912 <https://obito2912.github.io/se_project_aroundtheus/>
2025-02-19 22:01:37 -0700

    Start layout of last assignment of the block

---
+++ week07/ExerciseTracking/Exercise.cs
@@ -0,0 +1,9 @@
+public abstract class Exercise
+{
+    private DateOnly _date;
+    private int length;
+
+    public abstract void TrackActivity();
+
+    public abstract void
+}
---
+++ week07/ExerciseTracking/SwimmingExercise.cs
@@ -0,0 +1,4 @@
+public class SwimmingExercise : Exercise
+{
+    
+}
